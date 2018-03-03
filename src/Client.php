fe8e2149cf5f0cba19e7797ead205d97363ea1b3
Christofferson Viray <christoffersonviray@gmail.com>
2018-03-03 16:48:26 +0800

    added Client.php for connecting to ManagerIO

--- src/Client.php
+++ src/Client.php
@@ -4,4 +4,35 @@ namespace ManagerIO;
 
 class Client {
 	
+	//Base URL of where manager.io is
+	private $host;
+	private $username;
+	private $password;
+	private $businessKey;
+	
+	//ManagerIO generates this keys, it doesn't change
+	private $defautlKeys = [
+		'Customer' => 'ec37c11e-2b67-49c6-8a58-6eccb7dd75ee'
+	];
+	
+	public function __construct($host,$username,$password,$businessKey) {
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->businessKey = $businessKey;
+	}
+	
+	
+	public function add() {
+
+	}
+	
+	public function edit() {
+		
+	}
+	
+	public function delete() {
+		
+	}
+	
 }
