8acc7850b2827c8b0acd1e299b41d9add8d6f955
Christofferson Viray <christoffersonviray@gmail.com>
2018-03-03 16:49:35 +0800

    added Customer.php for customer entity

--- src/Customer.php
+++ src/Customer.php
@@ -2,6 +2,102 @@
 
 namespace ManagerIO;
 
+use GuzzleHttp\Client;
+
 class Customer {
+	private $name;							//string
+	private $billingAddress; 				//string
+	private $email;							//string(email)
+	private $businessIdentifier; 			//string
+	private $code; 							//string
+	private $inactive = false; 				//boolean
+	private $creditLimit; 					//float
+	private $hasStartingBalance; 			//boolean
+	private $startingBalanceType = 'Credit';//string
 	
+	//Constructors parameter keys are CASE-SENSITIVE
+	public function __construct($data = []) {
+		foreach($data as $key => $val) {
+			//Preprend "set" to the array key to match setter method
+			$method = 'set' . $key;
+			if(method_exists($this,$method)) {
+				//Set property if a setter method is found
+				$this->{$method}($val);
+			}
+		}
+	}
+	
+	//Setter and Getter Methods
+	
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getBillingAddress(){
+		return $this->billingAddress;
+	}
+
+	public function setBillingAddress($billingAddress){
+		$this->billingAddress = $billingAddress;
+	}
+
+	public function getEmail(){
+		return $this->email;
+	}
+
+	public function setEmail($email){
+		$this->email = $email;
+	}
+
+	public function getBusinessIdentifier(){
+		return $this->businessIdentifier;
+	}
+
+	public function setBusinessIdentifier($businessIdentifier){
+		$this->businessIdentifier = $businessIdentifier;
+	}
+
+	public function getCode(){
+		return $this->code;
+	}
+
+	public function setCode($code){
+		$this->code = $code;
+	}
+
+	public function getInactive(){
+		return $this->inactive;
+	}
+
+	public function setInactive($inactive){
+		$this->inactive = $inactive;
+	}
+
+	public function getCreditLimit(){
+		return $this->creditLimit;
+	}
+
+	public function setCreditLimit($creditLimit){
+		$this->creditLimit = $creditLimit;
+	}
+
+	public function getHasStartingBalance(){
+		return $this->hasStartingBalance;
+	}
+
+	public function setHasStartingBalance($hasStartingBalance){
+		$this->hasStartingBalance = $hasStartingBalance;
+	}
+
+	public function getStartingBalanceType(){
+		return $this->startingBalanceType;
+	}
+
+	public function setStartingBalanceType($startingBalanceType){
+		$this->startingBalanceType = $startingBalanceType;
+	}
 }
