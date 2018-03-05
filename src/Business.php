ce500b44f1ddc0f447a2847247bf1eb8fec3978a
Christofferson Viray <christoffersonviray@gmail.com>
2018-03-05 17:47:39 +0800

    added exceptions and comments

--- src/Business.php
+++ src/Business.php
@@ -3,35 +3,143 @@
 namespace ManagerIO;
 
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Exception\TooManyRedirectsException;
 
-class Business {	
-	//ManagerIO generates this keys, it doesn't change
+class Business {
+	/** @var array Generated IDs of categories */
 	private $defaultKeys = [
 		'Customer' => 'ec37c11e-2b67-49c6-8a58-6eccb7dd75ee'
 	];
 	
+	/**
+	* @param string $host        https://domain.com/api/
+	* @param string $username    Username in managerIO
+	* @param string $password    Password in managerIO
+	* @param string $businessKey XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+	*/
 	public function __construct($host,$username,$password,$businessKey) {
+		
 		$this->client = new GuzzleClient([
 			'base_uri' => $host.$businessKey.'/',
 			'auth' => [$username,$password]
 		]);
+		
 	}
 	
+	/**
+	* @param string $id
+	*
+	* @return json
+	*/
+	public function view($id) {
+		try{
+			
+			$response = $this->client->request('GET',$id.'.json');
+			
+			//Returns data in JSON format
+			return (string)$response->getBody();
+		
+		} catch(ClientException $e) {
+			//Client error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		} catch(ConnectException $e) {
+			echo 'Connection cannot be established.';
+			
+		} catch(ServerException $e) {
+			//Server error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		}
+	}
+	
+	/**
+	* @param object $object
+	*/
 	public function add($object) {
-		$classname = (new \ReflectionClass($object))->getShortName();
-		$this->client->request('POST',$this->defaultKeys[$classname],[
-			'json' => $object->get()
-		]);
+		try {
+			
+			$classname = (new \ReflectionClass($object))->getShortName();
+			$response = $this->client->request('POST',$this->defaultKeys[$classname],[
+				'json' => $object->get()
+			]);
+		
+		} catch(ClientException $e) {
+			//Client error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		} catch(ConnectException $e) {
+			echo 'Connection cannot be established.';
+			
+		} catch(ServerException $e) {
+			//Server error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		}
 	}
 	
-	public function edit($object) {
-		$this->client->request('PUT',$this->defaultKeys[get_class($object)],[
-			'json' => ['Name' => 'Sample Man']
-		]);
+	
+	/**
+	* @param string $id
+	* @param object $object
+	*/
+	public function edit($id,$object) {
+		try {
+			
+			$response = $this->client->request('PUT',$id,[
+				'json' => $object->get()
+			]);
+		
+		} catch(ClientException $e) {
+			//Client error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		} catch(ConnectException $e) {
+			//Connection cannot be established
+			echo 'Connection cannot be established.';
+			
+		} catch(ServerException $e) {
+			//Server error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		}
 	}
 	
-	public function delete($object) {
-		$this->client->request('DELETE','');
+	/**
+	* @param string $id
+	*/
+	public function delete($id) {
+		try {
+			
+			$response = $this->client->request('DELETE',$id.'.json');
+		
+		} catch(ClientException $e) {
+			//Client error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		} catch(ConnectException $e) {
+			//Connection cannot be established
+			echo 'Connection cannot be established.';
+			
+		} catch(ServerException $e) {
+			//Server error "Status code : Reason phrase"
+			echo $this->exceptionMessage($e);
+			
+		}
+	}
+	
+	/**
+	* Helper for the exception message 
+	*
+	* @param GuzzleHttp\Exception\ClientException | GuzzleHttp\Exception\ClientException $exception
+	*
+	* @return string
+	*/
+	private function exceptionMessage($e) {
+		return $e->getResponse()->getStatusCode() .' '. $e->getResponse()->getReasonPhrase();
 	}
 	
 }
